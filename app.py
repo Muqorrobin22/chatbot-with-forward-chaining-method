2b4febe067ba3e3b1eda0f252f6a4bbef5917b0b
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-24 16:12:48 +0700

    add CORS for local running

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from text_processing import textProcessing
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.get("/")
 def index_get():
